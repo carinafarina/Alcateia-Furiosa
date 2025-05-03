f4e1875cb683acfbe995d20b071a0364c46f4137
carina farina <41992311-carinafarina201@users.noreply.replit.com>
2025-05-03 04:35:40 +0000

    Atualizando o projeto final

--- main.py
+++ main.py
@@ -1,71 +1,55 @@
 import requests
-import time
 import json
 import os
 
-  
-class AlcateiaCSBot:
+
+class AlcateiaCSBot: 
   def __init__(self):
     token = '7710789467:AAGTDpqoetCT8X_hKat-QH-nH8o6-FRQDdA'
-    self.url_base = f'http://api.telegram.org/bot{token}/'
-    time.sleep(10)
-               
-  # Iniciar o bot
-
+    self.url_base = f'https://api.telegram.org/bot{token}/'
+   # iniciar bot
   def Iniciar(self):
     update_id = None
     while True:
-
       atualizacao = self.obter_mensagens(update_id)
       mensagens = atualizacao['result']
-
       if mensagens:
         for mensagem in mensagens:
           update_id = mensagem['update_id']
           chat_id = mensagem['message']['from']['id']
           eh_primeira_mensagem = mensagem['message']['message_id'] == 1
-          resposta = self.criar_resposta(mensagem,eh_primeira_mensagem)
+          resposta = self.criar_resposta(mensagem, eh_primeira_mensagem)
           self.responder(resposta, chat_id)
-
-# Obter mensagens
-
+    # obter mensagens
   def obter_mensagens(self, update_id):
     link_requisicao = f'{self.url_base}getUpdates?timeout=100'
     if update_id:
       link_requisicao = f'{link_requisicao}&offset={update_id + 1}'
     resultado = requests.get(link_requisicao)
     return json.loads(resultado.content)
-
-# Cria resposta
-
-  def criar_resposta(self,mensagem,eh_primeira_mensagem):
+    # criar resposta
+  def criar_resposta(self, mensagem, eh_primeira_mensagem):
     mensagem = mensagem['message']['text']
+    if eh_primeira_mensagem == True or mensagem.lower() == "opções":
+      return f'''Olá, bem vindo ao Alcateia! Digite o que você deseja saber sobre o Alcateia CS.{os.linesep}/Placar{os.linesep}/noticias{os.linesep}/proximojogo'''
+    if mensagem == '/Placar':
+      return f'''O placar da Alcateia CS é 2x1{os.linesep}Gostaria de saber mais alguma coisa?(s/n)'''
+    if mensagem == '/noticias':
+      return f'''Novo técnico será o do Furioso.{os.linesep}Gostaria de saber mais alguma coisa?(s/n)'''
+    if mensagem == '/proximojogo':
+      return f'''O proximo jogo é contra o time Furias{os.linesep}Gostaria de saber mais alguma coisa?(s/n)'''
     
-    if eh_primeira_mensagem == True or mensagem.lower() == 'menu':
-      return f'''Olá, bem vindo ao Alcateia Furiosa. 
-Digite sobre o que você quer saber:{os.linesep}placar{os.linesep}noticias{os.linesep}proximojogo'''
-      
-    if mensagem == 'placar':
-      return f'''O placar do jogo é 2x1{os.linesep} Gostaria de saber mais alguma coisa?(s/n)'''
-    if mensagem == 'noticias':
-      return f'''Aqui estão as noticias{os.linesep} Gostaria de saber mais alguma coisa?(s/n)'''
-    if mensagem == 'proximojogo':
-      return f'''O proximo jogo é contra o time Furias{os.linesep} Gostaria de saber mais alguma coisa?(s/n)'''
-
-    if mensagem.lower() in ('S','sim'):
-      return 'Digite sobre o que você quer saber:'
-
-     else:
-      return'Gostaria de acessar o menu?Digite "menu"  '
-
-    
-# responder
-
+    if mensagem.lower() in ('s','sim'):
+      return 'aguarde um momento...'
+    else: 
+      return 'Gostaria de acessar as outras opções? Digite "opções"' 
+  
+  
+  # responder
   def responder(self, resposta, chat_id):
-    #enviar
+    #enviar mensagem
     link_de_envio = f'{self.url_base}sendMessage?chat_id={chat_id}&text={resposta}'
     requests.get(link_de_envio)
-
 
 bot = AlcateiaCSBot()
 bot.Iniciar()
